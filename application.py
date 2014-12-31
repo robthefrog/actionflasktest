5cb2456d990e2f621200eb9e0b06eb95d784202e
Rob G <robthefrog@gmail.com>
2014-12-31 12:31:22 -0600

    make css static

--- application.py
+++ application.py
@@ -5,18 +5,11 @@ from flask import url_for
 from flask import request
 from flask import make_response
 from flask import flash
-from flask.ext.assets import Bundle
-from flask.ext.assets import Environment
 import json
 from db_controller import DB_Controller
 from user import User
 
 application = Flask(__name__)
-
-assets = Environment(application)
-assets.url = application.static_url_path
-scss = Bundle('all.scss', filters='scss', output='all.css')
-assets.register('scss_all', scss)
 
 db_controller = DB_Controller()
 
