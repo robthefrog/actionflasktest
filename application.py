09de98c71bd780484782ce4aae45499512b0770b
Robert Goddard <robert.goddard@kihomac.com>
2015-01-08 10:47:15 -0600

    Cleanup some comparisons

--- application.py
+++ application.py
@@ -14,17 +14,21 @@ def users_index():
 
 @application.route("/<api_key>/users")
 def users_json(api_key):
+    json = "{\"Error\": \"Invalid API key\"}"
+
     if (db_controller.valid_api_key(api_key)):
-        return db_controller.get_users_json()
-    else:
-        return "{\"Error\": \"Invalid API key\"}"
+        json = db_controller.get_users_json()
+
+    return json
 
 @application.route("/<api_key>/<int:user_id>/action_items")
 def action_items_for_user_json(api_key, user_id):
+    json = "{\"Error\": \"Invalid API key\"}"
+    
     if (db_controller.valid_api_key(api_key)):
-        return db_controller.get_action_items_for_user_json(user_id)
-    else:
-        return "{\"Error\": \"Invalid API key\"}"
+        json = db_controller.get_action_items_for_user_json(user_id)
+
+    return json
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
